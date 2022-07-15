3e26006c2ce330b387999e1e4d5e8b8f74dc0b64
WolfDevMC <91499295+wolfdev1@users.noreply.github.com>
2022-07-15 09:55:03 -0500

    Add some recomendations

--- tools/remote_administration.py
+++ tools/remote_administration.py
@@ -2,6 +2,14 @@
 from core import HackingTool
 from core import HackingToolsCollection
 
+#It is highly recommended to use the tools inside root
+
+#sudo su, this to improve efficiency, which is the goal of this tool
+
+
+#Avoid running tools while another python process is running 
+
+#since it can interfere or cause performance problems when we use the tools 
 
 class Stitch(HackingTool):
     TITLE = "Stitch"
