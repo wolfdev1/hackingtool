5845093b05e63c3927f4479bde5aacf3a2942409
WolfDevMC <91499295+wolfdev1@users.noreply.github.com>
2022-07-14 23:50:45 -0500

    Add some recomendations

--- tools/phising_attack.py
+++ tools/phising_attack.py
@@ -4,6 +4,12 @@ import os
 from core import HackingTool
 from core import HackingToolsCollection
 
+#It is highly recommended to use the tools inside root
+#sudo su, this to improve efficiency, which is the goal of this tool
+
+#Avoid running tools while another python process is running 
+#since it can interfere or cause performance problems when we use the tools 
+
 
 class Setoolkit(HackingTool):
     TITLE = "Setoolkit"
