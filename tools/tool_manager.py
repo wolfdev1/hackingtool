c6a4ba925e05977a940e22fa6ed4f8e862e772eb
WolfDevMC <91499295+wolfdev1@users.noreply.github.com>
2022-07-15 09:57:15 -0500

    Add some recomendations

--- tools/tool_manager.py
+++ tools/tool_manager.py
@@ -5,6 +5,11 @@ from time import sleep
 from core import HackingTool
 from core import HackingToolsCollection
 
+#It is highly recommended to use the tools inside root
+#sudo su, this to improve efficiency, which is the goal of this tool
+
+#Avoid running tools while another python process is running 
+#since it can interfere or cause performance problems when we use the tools 
 
 class UpdateTool(HackingTool):
     TITLE = "Update Tool or System"
