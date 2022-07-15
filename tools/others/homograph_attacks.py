23bce7c7be9c34c427535648833208ec6cb93234
WolfDevMC <91499295+wolfdev1@users.noreply.github.com>
2022-07-15 22:33:47 +0200

    Add some recomendations

--- tools/others/homograph_attacks.py
+++ tools/others/homograph_attacks.py
@@ -2,6 +2,11 @@
 from core import HackingTool
 from core import HackingToolsCollection
 
+#It is highly recommended to use the tools inside root
+#sudo su, this to improve efficiency, which is the goal of this tool
+
+#Avoid running tools while another python process is running 
+#since it can interfere or cause performance problems when we use the tools 
 
 class EvilURL(HackingTool):
     TITLE = "EvilURL"
