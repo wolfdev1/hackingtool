cc05c823e9b9eda1c0b2cdd129041742dd54fbc9
WolfDevMC <91499295+wolfdev1@users.noreply.github.com>
2022-07-14 23:53:35 -0500

    Add some recomendations

--- tools/information_gathering_tools.py
+++ tools/information_gathering_tools.py
@@ -8,6 +8,13 @@ from core import HackingTool
 from core import HackingToolsCollection
 from core import clear_screen
 
+#It is highly recommended to use the tools inside root
+
+#sudo su, this to improve efficiency, which is the goal of this tool
+
+#Avoid running tools while another python process is running 
+
+#since it can interfere or cause performance problems when we use the tools 
 
 class NMAP(HackingTool):
     TITLE = "Network Map (nmap)"
